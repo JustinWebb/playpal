948eede1df8ac4cf68f4e56635eb7c7b21bc150f
Justin Webb <jw@justinwebb.io>
2015-09-09 00:10:20 -0700

    Add JShint on reload

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,40 @@ var livereload = require('gulp-livereload');
 var path = require('path');
 var config = require('./build-config.js');
 
+
+
+
+var updateActiveProblem = function (filename) {
+  config.activeProblem = config.problemDir.concat('/'+ filename + '.js');
+};
+
 // Reload the specRunner.html file after changes to its related
 // Javascript, including spec and source files.
 gulp.task('default', function () {
 
   // Create Glob pattern to include test scripts and toy problem
   // source code.
-  config.toyProblemSrc = config.problemDir.concat('/**/*.js');
+  var toyProblemSrc = config.problemDir.concat('/**/*.js');
 
   // Start Livereload
   livereload.listen();
 
   // Make sure Gulp watches the actively-edited toy problem and
   // its support files and reloads the associated spec runner.
-  gulp.watch(config.toyProblemSrc, function (file) {
+  gulp.watch(toyProblemSrc, function (file) {
     var editedDirname =  path.basename(path.dirname(file.path));
     var specRunnerHTML = config.problemDir.concat('/'+ editedDirname +'/index.html');
+
+    updateActiveProblem(editedDirname);
     livereload.reload(specRunnerHTML);
   });
+});
+
+gulp.task('check-syntax', function () {
+
+  return gulp.src(config.activeProblem)
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
+    .pipe(jshint.reporter('fail'));
 });
 
