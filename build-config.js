aed56c4720b4032ce3f4573979506a00ca88f0dd
Justin Webb <jw@justinwebb.io>
2015-09-08 17:28:58 -0700

    Include external config file and refactor gulpfile for modularity

---
+++ build-config.js
@@ -0,0 +1,3 @@
+module.exports  = {
+  problemDir   : 'problems'
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,25 @@
 var gulp = require('gulp');
 var livereload = require('gulp-livereload');
 var path = require('path');
-var specRunnerPath = 'problems/[TARGET]/index.html'
-var config = {
-  toyProblems: ['problems/**/*.js']
-};
+var config = require('./build-config.js');
 
 // Reload the specRunner.html file after changes to its related
 // Javascript, including spec and source files.
 gulp.task('default', function () {
 
+  // Create Glob pattern to include test scripts and toy problem
+  // source code.
+  config.toyProblemSrc = config.problemDir.concat('/**/*.js');
+
+  // Start Livereload
   livereload.listen();
 
-  gulp.watch(config.toyProblems, function (file) {
+  // Make sure Gulp watches the actively-edited toy problem and
+  // its support files and reloads the associated spec runner.
+  gulp.watch(config.toyProblemSrc, function (file) {
     var editedDirname =  path.basename(path.dirname(file.path));
-    var specRunnerHTML = specRunnerPath.replace('[TARGET]', editedDirname);
-    console.log('Spec Runner: ', specRunnerHTML);
+    var specRunnerHTML = config.problemDir.concat('/'+ editedDirname +'/index.html');
     livereload.reload(specRunnerHTML);
   });
 });
+
